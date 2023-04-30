03e392f2b6189579e90a1ebebda952d8a971e708
Rishabh kumar <imrishabh9628@gmail.com>
2023-05-01 01:15:25 +0530

    Snake is following keyboard buttons

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
-let snakeX=5, snakeY=10;
+let velocityX = 0, velocityY = 0;
+let snakeX = 6, snakeY = 10;
 
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
-changeFoodPosition();
+
+
+const changeDirection = (e) => {
+    if (e.key === "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.key === "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    } else if (e.key === "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    } else if (e.key === "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }
+
+    initGame();
+
+}
+
 const initGame = () => {
+
     let html = `<div class="food" style="grid-area:${foodY}/${foodX} "></div>`;
-    html += `<div class="snake" style="grid-area:${snakeY}/${snakeX} "></div>`;
+
+    snakeX+=velocityX;
+    snakeY+=velocityY;
+
+    html += `<div class="snake-head" style="grid-area:${snakeY}/${snakeX} "></div>`;
     playBoard.innerHTML = html;
 }
-initGame(); 
+
+changeFoodPosition();
+initGame();
+
+document.addEventListener("keydown", changeDirection);
