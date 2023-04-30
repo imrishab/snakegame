27855646683ade3d94e9d39039aa96ef60f0f0b0
Rishabh kumar <imrishabh9628@gmail.com>
2023-04-30 23:06:07 +0530

    added snake

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
+let snakeX=5, snakeY=10;
 
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
@@ -9,8 +10,7 @@ const changeFoodPosition = () => {
 changeFoodPosition();
 const initGame = () => {
     let html = `<div class="food" style="grid-area:${foodY}/${foodX} "></div>`;
+    html += `<div class="snake" style="grid-area:${snakeY}/${snakeX} "></div>`;
     playBoard.innerHTML = html;
 }
 initGame(); 
-
-console.log(`math.random = ${math.random}`);
