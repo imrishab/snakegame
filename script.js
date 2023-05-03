dd6201278b9942b35df0b54cd8a43587acf9d156
Rishabh kumar <imrishabh9628@gmail.com>
2023-05-03 23:36:03 +0530

    snake game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
-let snakeBody=[];
+let snakeBody = [];
 let velocityX = 0, velocityY = 0;
 let snakeX = 6, snakeY = 10;
 
@@ -37,29 +37,31 @@ const initGame = () => {
 
     let html = `<div class="food" style="grid-area:${foodY}/${foodX} "></div>`;
 
-    snakeBody[0]=[snakeX, snakeY];
+    
 
     snakeX += velocityX;
     snakeY += velocityY;
-    
 
-for (let i = snakeBody.length-1; i >0; i--) {
-    snakeBody[i]=snakeBody[i-1];
+
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
     
-}
+    snakeBody[0] = [snakeX, snakeY];
 
     for (let i = 0; i < snakeBody.length; i++) {
         html += `<div class="snake-head" style="grid-area:${snakeBody[i][1]}/${snakeBody[i][0]} "></div>`;
-        
+
     }
-
-
     
+
+
     playBoard.innerHTML = html;
     if (snakeX === foodX && snakeY === foodY) {
         changeFoodPosition();
         snakeBody.push([foodX, foodY]);
     }
+
 }
 
 changeFoodPosition();
