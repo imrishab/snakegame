dc406d47a70b631d24403bca9a1eda9ba8809833
Rishabh kumar <imrishabh9628@gmail.com>
2023-05-06 03:52:50 +0530

    javascript file

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const playBoard = document.querySelector(".play-board");
+const playAgainButton = document.querySelector(".playagainbutton")
+const gameOverBox = document.querySelector(".gameoverbox")
+let gameOver = false;
+var score1 = document.querySelector(".score1");
+var score2 = document.querySelector(".score2");
+var max = 0;
+var highScore = document.querySelector(".high-score");
+
 
 let foodX, foodY;
 let snakeBody = [];
@@ -27,6 +35,8 @@ const changeDirection = (e) => {
     } else if (e.key === "ArrowLeft") {
         velocityX = -1;
         velocityY = 0;
+    } else if (gameOver) {
+        return;
     }
 
 
@@ -37,7 +47,7 @@ const initGame = () => {
 
     let html = `<div class="food" style="grid-area:${foodY}/${foodX} "></div>`;
 
-    
+
 
     snakeX += velocityX;
     snakeY += velocityY;
@@ -46,14 +56,14 @@ const initGame = () => {
     for (let i = snakeBody.length - 1; i > 0; i--) {
         snakeBody[i] = snakeBody[i - 1];
     }
-    
+
     snakeBody[0] = [snakeX, snakeY];
 
     for (let i = 0; i < snakeBody.length; i++) {
         html += `<div class="snake-head" style="grid-area:${snakeBody[i][1]}/${snakeBody[i][0]} "></div>`;
 
     }
-    
+
 
 
     playBoard.innerHTML = html;
@@ -62,10 +72,44 @@ const initGame = () => {
         snakeBody.push([foodX, foodY]);
     }
 
+    if (snakeX === 31 || snakeY === 31 || snakeX === 0 || snakeY === 0) {
+        gameOverBox.style.display = "flex";
+        document.removeEventListener("keydown", changeDirection);
+        gameover()
+
+    }
+    score1.innerHTML = `Score : ${snakeBody.length - 1}`;
+    score2.innerHTML = `Your score is : ${snakeBody.length - 1}`;
+
+    if (snakeBody.length - 1 > max) {
+        highScore.innerHTML = `High Score : ${(snakeBody.length - 1)}`
+        max = snakeBody.length - 1
+    }
+
+
+}
+const gameover = () => {
+    snakeX = 6;
+    snakeY = 10;
+    velocityX = 0;
+    velocityY = 0;
 }
 
 changeFoodPosition();
 
 setInterval(initGame, 125);
 
+function endGame() {
+    gameOver = true;
+
+    document.removeEventListener("keydown", changeDirection);
+}
+function playagain() {
+    gameOverBox.style.display = "none";
+    document.addEventListener("keydown", changeDirection);
+    snakeBody = [];
+}
+
 document.addEventListener("keydown", changeDirection);
+playAgainButton.addEventListener("click", playagain);
+
