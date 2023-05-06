594852732d1ccdca0544ba230e24db1749a8fed1
Rishabh kumar <imrishabh9628@gmail.com>
2023-05-06 13:56:34 +0530

    final commit

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const playBoard = document.querySelector(".play-board");
-const playAgainButton = document.querySelector(".playagainbutton")
-const gameOverBox = document.querySelector(".gameoverbox")
+
+const playAgainButton = document.querySelector(".playagainbutton");
+
+const gameOverBox = document.querySelector(".gameoverbox");
+
 let gameOver = false;
+
 var score1 = document.querySelector(".score1");
+
 var score2 = document.querySelector(".score2");
+
 var max = 0;
+
 var highScore = document.querySelector(".high-score");
 
-const up=document.querySelector(".up");
-const down=document.querySelector(".down");
-const right=document.querySelector(".right");
-const left=document.querySelector(".left");
+const up = document.querySelector(".up");
+
+const down = document.querySelector(".down");
+
+const right = document.querySelector(".right");
+
+const left = document.querySelector(".left");
 
 
 
 let foodX, foodY;
+
 let snakeBody = [];
+
 let velocityX = 0, velocityY = 0;
+
 let snakeX = 6, snakeY = 10;
 
 const changeFoodPosition = () => {
@@ -60,11 +73,14 @@ const initGame = () => {
     snakeY += velocityY;
 
 
+
     for (let i = snakeBody.length - 1; i > 0; i--) {
         snakeBody[i] = snakeBody[i - 1];
     }
 
+
     snakeBody[0] = [snakeX, snakeY];
+
 
     for (let i = 0; i < snakeBody.length; i++) {
         html += `<div class="snake-head" style="grid-area:${snakeBody[i][1]}/${snakeBody[i][0]} "></div>`;
@@ -79,14 +95,20 @@ const initGame = () => {
         snakeBody.push([foodX, foodY]);
     }
 
+
     if (snakeX === 31 || snakeY === 31 || snakeX === 0 || snakeY === 0) {
         gameOverBox.style.display = "flex";
         document.removeEventListener("keydown", changeDirection);
         gameover()
 
     }
+
+
     score1.innerHTML = `Score : ${snakeBody.length - 1}`;
+
+
     score2.innerHTML = `Your score is : ${snakeBody.length - 1}`;
+
 
     if (snakeBody.length - 1 > max) {
         highScore.innerHTML = `High Score : ${(snakeBody.length - 1)}`
@@ -97,8 +119,11 @@ const initGame = () => {
 }
 const gameover = () => {
     snakeX = 6;
+
     snakeY = 10;
+
     velocityX = 0;
+    
     velocityY = 0;
 }
 
@@ -111,31 +136,41 @@ function endGame() {
 
     document.removeEventListener("keydown", changeDirection);
 }
+
 function playagain() {
     gameOverBox.style.display = "none";
     document.addEventListener("keydown", changeDirection);
     snakeBody = [];
 }
-function goup(){
+
+function goup() {
     velocityX = 0;
     velocityY = -1;
 }
-function godown(){
+
+function godown() {
     velocityX = 0;
     velocityY = 1;
 }
-function goright(){
+
+function goright() {
     velocityX = 1;
     velocityY = 0;
 }
-function goleft(){
+
+function goleft() {
     velocityX = -1;
-        velocityY = 0;
+    velocityY = 0;
 }
 
 document.addEventListener("keydown", changeDirection);
+
 playAgainButton.addEventListener("click", playagain);
+
 up.addEventListener("click", goup)
+
 down.addEventListener("click", godown)
+
 right.addEventListener("click", goright)
+
 left.addEventListener("click", goleft)
